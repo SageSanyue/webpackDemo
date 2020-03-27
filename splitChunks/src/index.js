fe699dd511acf2b9d1727361e3373ff49ae297d9
SageSanyue <562441461@qq.com>
2020-03-28 01:03:42 +0800

    splitchunks实例

---
+++ splitChunks/src/index.js
@@ -0,0 +1,8 @@
+import { sum } from './math'
+
+const sumRes = sum(10, 20)
+console.log('sumRes', sumRes)
+
+// 引入第三方模块
+import _ from 'lodash'
+console.log(_.each, 8)
---
+++ splitChunks/src/other.js
@@ -0,0 +1,7 @@
+import { sum, mult } from './math'
+
+const sumRes = sum(10, 20)
+
+console.log('other-sumRes', sumRes)
+console.log('other page', mult(10, 20))
+
