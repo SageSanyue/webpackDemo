d2e917866d5e4432c7971f0ffef50aab08102604
SageSanyue <562441461@qq.com>
2020-03-27 23:24:17 +0800

    重来

--- splitChunks/src/a.js
+++
@@ -1,8 +0,0 @@
-import react from './node_modules/react';
-import $ from './node_modules/jquery';
-import(/* webpackChunkName: "a-lodash" */'./node_modules/lodash');
-import b from './b'
-
-console.log('a', b)
-const a = 'I am a.js';
-export default a;
--- splitChunks/src/b.js
+++
@@ -1,6 +0,0 @@
-import $ from './node_modules/jquery';
-import(/* webpackChunkName: "b-react" */'./node_modules/react');
-import(/* webpackChunkName: "b-lodash" */'./node_modules/lodash');
-
-const b = 'I am b.js';
-export default b;
