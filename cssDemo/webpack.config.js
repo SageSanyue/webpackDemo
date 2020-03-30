be46ded997f52c33f140c3a11e395ad7b0621baf
SageSanyue <562441461@qq.com>
2020-03-31 00:03:37 +0800

    mini-css-extract-plugin改变style-loader的<style>为<link>

--- cssDemo/webpack.config.js
+++ cssDemo/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 let HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = {
     entry: './src/index.js',
@@ -12,7 +14,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 
+                    MiniCssExtractPlugin.loader, // 更换style-loader
                     {
                         loader: 'css-loader',
                         options: {
@@ -24,6 +26,11 @@ module.exports = {
         ]
     },
     plugins: [
+        // 添加 plugin
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
         new HtmlWebpackPlugin({
             // 用哪个html作为模板
             // 在src目录下创建一个index.html页面当做模板来用
